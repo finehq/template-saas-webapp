1b82d0ada319d10570a271aaccd0fc83389fd88c
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-09 14:50:25 +0400

    update template

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes } from "react-router";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes> </Routes>
+  </BrowserRouter>
+);
