3b369d09bc7da1ad6463686ae82bc727e9878ead
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-09 19:57:30 +0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes } from "react-router";
-
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
