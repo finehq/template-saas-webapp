349098ea19c5e1fad5db9bd985fce455b07ac5f9
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-21 14:43:41 +0400

    ui components update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes } from "react-router";
+
+import { TooltipProvider } from "./components/ui/tooltip";
+
 import "./index.css";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes> </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <BrowserRouter>
+        <Routes> </Routes>
+      </BrowserRouter>
+      <Sonner />
+      <Toaster />
+    </TooltipProvider>
+  </QueryClientProvider>
 );
