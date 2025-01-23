1a3086e07dcf2857907e85f48cee2c65aff5bb4b
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-23 16:06:08 +0400

    update template metadata

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainPage from "./pages";
 
 import { TooltipProvider } from "./components/ui/tooltip";
 
@@ -13,7 +14,9 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <BrowserRouter>
-        <Routes> </Routes>
+        <Routes>
+          <Route path="/" Component={MainPage} />
+        </Routes>
       </BrowserRouter>
       <Sonner />
       <Toaster />
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Banner from "@/components/banner";
+const MainPage = () => {
+  return (
+    <div className="min-h-screen w-full">
+      <Banner />
+    </div>
+  );
+};
+
+export default MainPage;
