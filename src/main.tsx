5ca6c357f4ee271acce9026e5ae343ddc7a25904
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-24 16:24:37 +0400

    pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+import Banner from "@/components/Banner";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
@@ -6,7 +8,6 @@ import { BrowserRouter, Routes } from "react-router";
 
 import { TooltipProvider } from "./components/ui/tooltip";
 
-import Banner from "./components/Banner";
 import "./index.css";
 const queryClient = new QueryClient();
 
