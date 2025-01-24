242ee8560693128fcb6e40f85d56d5c802095745
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-24 16:21:22 +0400

    update template

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import MainPage from "./pages";
+import { BrowserRouter, Routes } from "react-router";
 
 import { TooltipProvider } from "./components/ui/tooltip";
 
+import Banner from "./components/Banner";
 import "./index.css";
 const queryClient = new QueryClient();
 
@@ -14,12 +14,11 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={MainPage} />
-        </Routes>
+        <Routes></Routes>
       </BrowserRouter>
       <Sonner />
       <Toaster />
+      <Banner />
     </TooltipProvider>
   </QueryClientProvider>
 );
