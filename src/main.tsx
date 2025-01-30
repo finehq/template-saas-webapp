2d0d2f321cf14ad98fa9268613e28e365e6db587
Saba Kobaidze <sbkobaidze@gmail.com>
2025-01-30 14:45:34 +0400

    update template

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
+// start the app always with '/' route
 import Banner from "@/components/layout/Banner";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 import { TooltipProvider } from "./components/ui/tooltip";
 
 import { SidebarProvider } from "./components/ui/sidebar";
 import "./index.css";
+import Index from "./pages";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -17,7 +19,9 @@ createRoot(document.getElementById("root")!).render(
     <SidebarProvider>
       <TooltipProvider>
         <BrowserRouter>
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Index />} />
+          </Routes>
         </BrowserRouter>
         <Sonner />
         <Toaster />
