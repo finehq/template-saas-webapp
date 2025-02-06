00bf99c749a2862507952bffe8a5fc4ef8e2d99a
Saba Kobaidze <sbkobaidze@gmail.com>
2025-02-06 22:17:45 +0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -10,25 +10,26 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { TooltipProvider } from "./components/ui/tooltip";
 
 import { ThemeProvider } from "./components/layout/theme-provider";
+import { SidebarProvider } from "./components/ui/sidebar";
 import "./index.css";
 import Index from "./pages";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    {/* <SidebarProvider> */}
-    <TooltipProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-          </Routes>
-        </BrowserRouter>
-        <Sonner />
-        <Toaster />
-        <Banner />
-      </ThemeProvider>
-    </TooltipProvider>
-    {/* </SidebarProvider> */}
+    <SidebarProvider>
+      <TooltipProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+            </Routes>
+          </BrowserRouter>
+          <Sonner />
+          <Toaster />
+          <Banner />
+        </ThemeProvider>
+      </TooltipProvider>
+    </SidebarProvider>
   </QueryClientProvider>
 );
