01f8548c3cc1a5867b29c3c7d7ad2d032142aa7a
Saba Kobaidze <sbkobaidze@gmail.com>
2025-02-06 21:49:16 +0400

    remove layout component

--- src/components/layout/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="h-screen w-full bg-background container">{children}</div>
-  );
-};
-
-export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 // Update this page (the content is just a fallback if you fail to update the page)
 
-import Layout from "@/components/layout/Layout";
-
 const Index = () => {
   return (
-    <Layout>
-      <div className=" h-full flex items-center justify-center bg-gray-100 ">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Welcome to Your Blank App</h1>
-          <p className="text-xl text-gray-600">
-            Start building your amazing project here!
-          </p>
-        </div>
+    <div className=" min-h-screen w-full flex items-center justify-center bg-gray-100 ">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Your Blank App</h1>
+        <p className="text-xl text-gray-600">
+          Start building your amazing project here!
+        </p>
       </div>
-    </Layout>
+    </div>
   );
 };
 
