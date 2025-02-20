502e2e4e60c023f9bb4d11478b1372cc787e7b01
Saba Kobaidze <sbkobaidze@gmail.com>
2025-02-20 17:57:37 +0400

    disable strict type checks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 // Update this page (the content is just a fallback if you fail to update the page). Always include w-full and min-h-screen classes in the main element.
-import React from 'react'
 const Index = () => {
   return (
     <main className="  flex items-center justify-center bg-gray-100 ">
