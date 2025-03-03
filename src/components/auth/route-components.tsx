14e14c97fcda781a2413129cd7de154abea37a7e
Yotam Haran <yotam@fine.dev>
2025-03-03 10:16:40 +0200

    Added auth pages + FineClient integration

---
+++ src/components/auth/route-components.tsx
@@ -0,0 +1,13 @@
+import { useFine } from "@/hooks/use-fine";
+import { PropsWithChildren } from "react";
+import { Navigate, Outlet, Route } from "react-router";
+
+export const GuestRoute = ({ Component }: { Component: () => JSX.Element }) => {
+  const fine = useFine();
+  return fine.user ? <Navigate to='/' /> : <Component />;
+};
+
+export const AuthenticatedRoute = ({ Component }: { Component: () => JSX.Element }) => {
+  const fine = useFine();
+  return fine.authLoaded && !fine.user ? <Navigate to='/login' /> : <Component />;
+};
