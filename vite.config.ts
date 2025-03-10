386b4b9255f4881a517a75def634d600b964247f
Saba Kobaidze <sbkobaidze@gmail.com>
2025-03-10 15:40:02 +0400

    include sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
