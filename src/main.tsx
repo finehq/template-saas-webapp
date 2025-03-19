9d35c6e02925a18402e2a29bf60038e83b21d766
Yotam Haran <yotam@fine.dev>
2025-03-19 11:07:26 +0200

    Removed auth from template for now

--- src/main.tsx
+++ src/main.tsx
@@ -8,33 +8,23 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import { TooltipProvider } from "./components/ui/tooltip";
 
-import { AuthenticatedRoute, GuestRoute } from "./components/auth/route-components";
 import { ThemeProvider } from "./components/layout/theme-provider";
-import { FineProvider } from "./hooks/use-fine";
 import "./index.css";
 import Index from "./pages";
-import LoginPage from "./pages/auth/login";
-import LogoutPage from "./pages/auth/logout";
-import SignupPage from "./pages/auth/signup";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <FineProvider>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <ThemeProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Index />} />
-              <Route path='/login' element={<GuestRoute Component={LoginPage} />} />
-              <Route path='/signup' element={<GuestRoute Component={SignupPage} />} />
-              <Route path='/logout' element={<AuthenticatedRoute Component={LogoutPage} />} />
-            </Routes>
-          </BrowserRouter>
-          <Sonner />
-          <Toaster />
-        </ThemeProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </FineProvider>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Index />} />
+          </Routes>
+        </BrowserRouter>
+        <Sonner />
+        <Toaster />
+      </ThemeProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
