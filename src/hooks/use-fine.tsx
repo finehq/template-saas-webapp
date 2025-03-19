83c33fbff721519271527a519ab976c89d6c481d
Yotam Haran <yotam@fine.dev>
2025-03-19 10:52:18 +0200

    Changes to support sdk updates

--- src/hooks/use-fine.tsx
+++ src/hooks/use-fine.tsx
@@ -1,6 +1,6 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useLayoutEffect, useState } from "react";
-import { FineClient, User } from "@fine-dev/fine-js";
-import { fine } from "@/lib/fine";
+
+type User = unknown;
 
 type FineContextValue = {
   /** Available if the user is authenticated */
@@ -15,18 +15,18 @@ const FineContext = createContext(initialFineContext);
 export const useFine = () => useContext(FineContext);
 
 /**
- * If you have a `FineClient` set up, this provider will provide the client for use in the app, as well as load the authenticated user into the context.
+ * If you have a `FineClient` set up, this provider will load the authenticated user into the context.
  */
 export const FineProvider = ({ children }: PropsWithChildren) => {
   const [context, setContext] = useState(initialFineContext);
 
-  useLayoutEffect(() => {
-    const listener = fine?.auth.onAuthStateChange(async (_, session) => {
-      setContext((state) => ({ ...state, user: session?.user ?? null, authLoaded: true }));
-    });
+  // useLayoutEffect(() => {
+  //   const listener = fine?.auth.onAuthStateChange(async (_, session) => {
+  //     setContext((state) => ({ ...state, user: session?.user ?? null, authLoaded: true }));
+  //   });
 
-    return () => listener?.data.subscription?.unsubscribe();
-  }, []);
+  //   return () => listener?.data.subscription?.unsubscribe();
+  // }, []);
 
   return <FineContext.Provider value={context}>{children}</FineContext.Provider>;
 };
