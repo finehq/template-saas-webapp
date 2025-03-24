b8a55a7c440536325dacba94427d252e83e1afe6
Dan Leshem <dan@leshem.co>
2025-03-24 08:05:47 +0200

    added auth components and upgrade fine sdk

--- src/lib/fine.ts
+++ src/lib/fine.ts
@@ -3,4 +3,4 @@ import { FineClient } from "@fine-dev/fine-js";
 /**
  * This variable will hold the Fine client. It will be populated automatically by Fine when you have a backend set up
  */
-export const fine = new FineClient("https://40489032-1500-458e-a076-d41d11266e00.baas.fine.dev")
+export const fine: FineClient | null = null;
